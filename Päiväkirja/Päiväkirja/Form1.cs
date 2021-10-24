bcfc8db964c9b4db2b43178348a0333c7ca09ec5
muhis12 <70624954+muhis12@users.noreply.github.com>
2021-10-24 19:21:28 +0300

    Add files via upload

---
+++ Päiväkirja/Päiväkirja/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Päiväkirja
+{
+    public partial class DiaryForm : Form
+    {
+        public DiaryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TallennaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = File.ReadAllText(@"C:\\Users\\Justus\\Desktop\\Uusi kansio\\T1-tason_ohjelmointi\\Päiväkirja\\paivakirja.txt");
+            teksti += SyottoTB.Text;
+            teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";
+            TextWriter text = new StreamWriter("C:\\Users\\Justus\\Desktop\\Uusi kansio\\T1-tason_ohjelmointi\\Päiväkirja\\paivakirja.txt");
+            text.Write(teksti);
+            text.Close();
+            Application.Exit();
+        }
+    }
+}
