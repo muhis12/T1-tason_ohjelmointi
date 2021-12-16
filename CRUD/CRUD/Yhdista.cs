678105fed7678f63c556373eb1a0ac4e16f8fb7f
muhis12 <70624954+muhis12@users.noreply.github.com>
2021-12-16 10:21:12 +0200

    Add files via upload

---
+++ CRUD/CRUD/Yhdista.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace CRUD
+{
+  class yhdista
+    {
+        public string Yhteyslause()
+        {
+            return "datasource=localhost; port=3306;username=root;password=;database=hotelli";
+        }
+        private MySqlConnection yhteys = new MySqlConnection("datasource=localhost; port=3306;username=root;password=;database=hotelli; SSL Mode = None");
+
+        public MySqlConnection otaYhteys()
+        {
+            return yhteys;
+        }
+        public void avaaYhteys()
+        {
+            if (yhteys.State == ConnectionState.Closed)
+            {
+                yhteys.Open();
+            }
+
+        }  
+        public void suljeYhteys()
+        {
+            if (yhteys.State == ConnectionState.Open)
+            {
+                yhteys.Close();
+            }
+        }
+    }
+}
